fbc185975d8ba67c0812bb065e50ec299e5702e5
KATYROFFE <KATYROFFE@GMAIL.COM>
2021-12-10 08:39:20 -0800

    Adds combines reducers. Updates data for categories and products.

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import { createStore, combineReducers } from 'redux';
 
 import { composeWithDevTools } from 'redux-devtools-extension';
+import cartReducer from './cart.js';
 import categoriesReducer from './categories.js';
 import productReducer from './products.js';
 
 const reducers = combineReducers({
   categories: categoriesReducer,
   products: productReducer,
+  cart: cartReducer,
 })
 
 
--- src/store/products.js
+++ src/store/products.js
@@ -2,12 +2,12 @@ const initialState = {
   
   // books, pets, novelty
   productList: [
-    {_id: '1', name: 'pet1', category: 'Pets', price: 5.99},
-    {_id: '2', name: 'pet2', category: 'Pets', price: 10.99},
-    {_id: '3', name: 'book1', category: 'Books', price: 7.99},
-    {_id: '4', name: 'book2', category: 'Books', price: 12.99},
-    {_id: '5', name: 'novelty1', category: 'Novelty', price: 100.49},
-    {_id: '6', name: 'novelty2', category: 'Novelty', price: 25.00},
+    {_id: '1', name: 'pet1', category: 'Pets', price: 5.99, inStock: 100},
+    {_id: '2', name: 'pet2', category: 'Pets', price: 10.99, inStock: 100},
+    {_id: '3', name: 'book1', category: 'Books', price: 7.99, inStock: 100},
+    {_id: '4', name: 'book2', category: 'Books', price: 12.99, inStock: 100},
+    {_id: '5', name: 'novelty1', category: 'Novelty', price: 100.49, inStock: 100},
+    {_id: '6', name: 'novelty2', category: 'Novelty', price: 25.00, inStock: 100},
   ],
   activeProduct: '',
   products: [],
