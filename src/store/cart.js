bce4f7ed6b862b83f3b8a150054536995b9d3bc5
KATYROFFE <KATYROFFE@GMAIL.COM>
2021-12-12 22:46:04 -0800

    Adds remove from cart feature.

--- src/store/cart.js
+++ src/store/cart.js
@@ -8,6 +8,19 @@ export default function cartReducer(state = initialState, action) {
   switch(type) {
     case 'ADD_TO_CART':
       return { ...state, cart: [ ...state.cart, payload] }
+    
+    case 'REMOVE_FROM_CART':
+      let cart = [...state.cart]
+      let deleteOne = true;
+      let newCart = cart.filter(item => {
+        if (item === payload && deleteOne) {
+          deleteOne = false;
+          return false;
+        } else {
+          return true;
+        }
+      })
+        return { ...state, cart: [...newCart]}
     default:
       return state;
   }
@@ -16,6 +29,13 @@ export default function cartReducer(state = initialState, action) {
 export function addToCart(name) {
   return {
     type: 'ADD_TO_CART',
+    payload: name,
+  }
+}
+
+export function removeFromCart(name) {
+  return {
+    type: 'REMOVE_FROM_CART',
     payload: name,
   }
 }
